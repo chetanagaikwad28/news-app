748b8681bfb288fc527fcd99daae0f05318e0fa5
chetanagaikwad28 <chetanagaikwad28@gmail.com>
2024-11-14 11:59:24 +0530

    hosted server

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust this URL based on your backend server configuration
+  baseURL: 'https://news-backend-w6c2.onrender.com/api', // Adjust this URL based on your backend server configuration
 });
 
 export default axiosInstance;
